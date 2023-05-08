3ec98746a9a0cf15bb38c3badd368921aec5b2a8
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-05-08 15:47:23 +0000

    rename module name

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -62,7 +62,7 @@ module "iam_awslbc" {
   aws_iam_openid_connect_provider_arn_split = element(split("oidc-provider/","${aws_iam_openid_connect_provider.eks_cluster.arn}"),1)
 }
 
-module "aws-load-balancer-controller" {
+module "aws-load-balancer-controller-helm" {
   source = "../modules/helm_charts/aws-load-balancer-controller"
   depends_on = [
     module.eks_cluster,
