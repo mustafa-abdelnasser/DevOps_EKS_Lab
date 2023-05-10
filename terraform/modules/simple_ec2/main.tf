339a94068206356e0eaa4d176bf004942bae0494
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-05-10 21:30:43 +0000

    ec2 security group

--- terraform/modules/simple_ec2/main.tf
+++ terraform/modules/simple_ec2/main.tf
@@ -49,6 +49,6 @@ resource "aws_instance" "ec2" {
   instance_type = var.ec2_instance_type
   subnet_id = var.subnet_id
   key_name = aws_key_pair.ec2_pub_key.key_name
-  security_groups = [ aws_security_group.public ]
+  vpc_security_vpc_security_group_ids = [ aws_security_group.public.id ]
 }
 
