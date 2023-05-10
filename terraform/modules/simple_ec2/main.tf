a59d4cb6dc6fb24e5ff9049d198fe44f7d2fb8bc
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-05-10 20:38:35 +0000

    fix module

--- terraform/modules/simple_ec2/main.tf
+++ terraform/modules/simple_ec2/main.tf
@@ -41,13 +41,13 @@ resource "aws_security_group_rule" "public_ssh" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
-    security_group_id = aws_security_group.public
+    security_group_id = aws_security_group.public.id
 }
 
 resource "aws_instance" "ec2" {
-  ami = data.aws_ami.os
+  ami = data.aws_ami.os.image_id
   instance_type = var.ec2_instance_type
   subnet_id = var.subnet_id
-  key_name = aws_key_pair.ec2_pub_key
+  key_name = aws_key_pair.ec2_pub_key.key_name
 }
 
