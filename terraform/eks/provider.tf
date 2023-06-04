11fab30a34134d6e94580af9135493eea04fcbad
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-06-04 18:29:27 +0000

    use eks data source

--- terraform/eks/provider.tf
+++ terraform/eks/provider.tf
@@ -29,23 +29,34 @@ provider "aws" {
 }
 
 # helm provider
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+  depends_on = [ module.eks_cluster ]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+  depends_on = [ module.eks_cluster ]
+}
+
 provider "helm" {
   kubernetes {
-    host = module.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority_data)
-    token = module.eks_cluster.cluster_auth.token
+    host = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
 provider "kubernetes" {
-  host = module.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority_data)
-  token = module.eks_cluster.cluster_auth.token
+  host = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "kubectl" {
   host = module.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority_data)
-  token = module.eks_cluster.cluster_auth.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.cluster.token
   load_config_file = false
 }
