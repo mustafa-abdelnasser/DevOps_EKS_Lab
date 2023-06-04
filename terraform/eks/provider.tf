338a06a6f29cb02a536cad8883065e2135733fb4
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-06-04 20:54:55 +0000

    use eks data source

--- terraform/eks/provider.tf
+++ terraform/eks/provider.tf
@@ -43,20 +43,20 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "helm" {
   kubernetes {
     host = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
 provider "kubernetes" {
   host = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "kubectl" {
   host = module.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token = data.aws_eks_cluster_auth.cluster.token
   load_config_file = false
 }
