9f4c4f433ce864caab8d35326ff63103f9a73f5c
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-06-04 19:16:30 +0000

    enforce https endpoint

--- terraform/modules/opensearch/main.tf
+++ terraform/modules/opensearch/main.tf
@@ -189,6 +189,11 @@ resource "aws_opensearch_domain" "cluster" {
     enabled = true
   }
 
+  domain_endpoint_options {
+    enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
+
   advanced_security_options {
     enabled = true
     internal_user_database_enabled = true
