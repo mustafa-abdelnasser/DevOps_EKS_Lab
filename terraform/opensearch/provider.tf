b0d9b03b34e14e2236b007c9053c5c85b0149baf
mustafa-abdelnasser <mustafa.abdelnasser@gmail.com>
2023-06-10 15:18:17 +0000

    add helm

--- terraform/opensearch/provider.tf
+++ terraform/opensearch/provider.tf
@@ -27,3 +27,23 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
+# helm provider
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+  depends_on = [ module.eks_cluster ]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+  depends_on = [ module.eks_cluster ]
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
